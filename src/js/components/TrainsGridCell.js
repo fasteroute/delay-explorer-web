d0c45688a27bfca82eb0fe0889339ad4d9f0038f
Joe Letts <joe@fasteroute.com>
2015-09-05 12:52:39 +0100

    Uses d3 to creat rudimentary color palette for TrainsGridCell

--- src/js/components/TrainsGridCell.js
+++ src/js/components/TrainsGridCell.js
@@ -1,14 +1,28 @@
 "use strict";
 
 var React = require('react');
+var d3 = require('d3');
+var color = d3.scale.quantize().range([
+  "rgb(253,204,138)",
+  "rgb(252,141,89)",
+  "rgb(227,74,51)",
+  "rgb(179,0,0)"]);
+color.domain([0, 30]);
+console.log(color);
 
 var TrainsGridCell = React.createClass({
-
   render: function() {
 
+    var lateColor;
+    if (this.props.lateness.average_lateness === 0.0) {
+      lateColor = "green";
+    } else {
+      lateColor = color(this.props.lateness.average_lateness);
+    }
     return (
-      <td>
-       {this.props.lateness.average_lateness}
+      <td style={{ padding: "5px" }}>
+        <div style={{ borderRadius: "25%", width: "50px", height: "50px", backgroundColor: lateColor }}>
+       </div>
       </td>
     );
   }
