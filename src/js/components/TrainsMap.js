49dd6209913504709463f1be12fa68b5bc791f31
George Goldberg <george@fasteroute.com>
2015-09-07 15:19:24 +0100

    Show stations on the map.

--- src/js/components/TrainsMap.js
+++ src/js/components/TrainsMap.js
@@ -1,18 +1,34 @@
 "use strict";
 
+// React
 var React = require('react');
 
+// Mixins
+var FluxMixin = require('fluxxor').FluxMixin(React);
+var StoreWatchMixin = require('fluxxor').StoreWatchMixin;
+
+// 3rd Party Components.
 var LeafletMap = require('react-leaflet').Map;
 var Marker = require('react-leaflet').Marker;
 var Popup = require('react-leaflet').Popup;
 var TileLayer = require('react-leaflet').TileLayer;
 
 var TrainsMap = React.createClass({
+
+  mixins: [FluxMixin, StoreWatchMixin('StationsStore')],
+
   getInitialState: function() {
     return {
       lat: 51.505,
       lng: -0.09,
       zoom: 12
+    };
+  },
+
+  getStateFromFlux: function() {
+    var stationsStore = this.getFlux().store('StationsStore');
+    return {
+      stations: stationsStore.stations 
     };
   },
 
@@ -23,6 +39,15 @@ var TrainsMap = React.createClass({
         url='http://tile.stamen.com/toner/{z}/{x}/{y}.png'
         attribution='Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://maps.stamen.com">Stamen</a>'
       />
+      {this.state.stations.map(function(station) {
+        return (
+          <Marker key={station.id} position={[station.lat, station.lon]}>
+            <Popup>
+              <h4>{station.name}</h4>
+            </Popup>
+          </Marker>
+        );
+      })}
     </LeafletMap>;
  }
 
