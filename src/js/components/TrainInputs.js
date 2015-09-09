32451605f0c5af8bd6c50a8f5737879f3ca56b76
Joe Letts <joe@fasteroute.com>
2015-09-09 20:54:14 +0100

    Initial commit of AutoComplete solution
    - Uses react-autosuggest
    - Currently functional only, no massive UI changes
    - Caches previous results :)
    - Stores CRS is the <input value> attribute
    
    Replaces inputs with autoSuggest inputs in TrainInput.js
    
    - Hooks up autocomplete to the component state

--- src/js/components/TrainInputs.js
+++ src/js/components/TrainInputs.js
@@ -1,13 +1,12 @@
 "use strict";
 
 var React = require('react/addons');
-
+var AutoSuggest = require('react-autosuggest');
 var NavigationMixin = require('react-router').Navigation;
-
+var $ = require('jquery');
 var ButtonInput = require('react-bootstrap').ButtonInput;
 var Input = require('react-bootstrap').Input;
 var Panel = require('react-bootstrap').Panel;
-
 
 var TrainInputs = React.createClass({
   mixins: [NavigationMixin, React.addons.LinkedStateMixin],
@@ -23,27 +22,52 @@ var TrainInputs = React.createClass({
     this.state.type = this.props.type;
     this.state.time = this.props.time;
   },
+  suggestions: function(input, callback) {
+    console.log('searching for ' + input);
+    $.getJSON('http://localhost:8080/Locations/' + encodeURIComponent(input), function(json) {
+      callback(null, json.locations);
+    })
+    .fail( function() {
+      callback(new Error("Couldn't get autocomplete"));
+    });
 
+  },
+  suggestionRenderer: function(suggestion, input) {
+    return suggestion.name;
+  },
+  suggestionValue: function(suggestionObj) {
+    return suggestionObj.name + " [" + suggestionObj.user_code + "]";
+  },
+  onOriginSelected: function(suggestion, event) {
+    event.preventDefault();
+    this.setState({from: suggestion.user_code});
+  },
+  onDestinationSelected: function(suggestion, event) {
+    event.preventDefault();
+    this.setState({to: suggestion.user_code});
+  },
   render: function() {
     console.log("Rendering TrainInputs.");
     return (
       <div className="section" id="start">
         <Panel className="InputsPanel">
           <form className="form-horizontal">
-            <Input type="text"
-                   ref="fromInput"
-                   label="From"
-                   valueLink={this.linkState('from')}
-                   placeholder="London Bridge"
-                   labelClassName="col-sm-1"
-                   wrapperClassName="col-sm-11"/>
-            <Input type="text"
-                   ref="toInput"
-                   label="To"
-                   valueLink={this.linkState('to')}
-                   placeholder="East Croydon"
-                   labelClassName="col-sm-1"
-                   wrapperClassName="col-sm-11"/>
+             <div className="form-group">
+               <label className="control-label col-sm-1" htmlFor='originAutoSuggest'>From</label>
+               <AutoSuggest suggestions={this.suggestions}
+                     suggestionRenderer={this.suggestionRenderer}
+                     suggestionValue={this.suggestionValue}
+                     onSuggestionSelected={this.onOriginSelected}
+                     inputAttributes={{ className: "col-sm-12 form-control", placeholder: "London Bridge", id: "originAutoSuggest"}}/>
+            </div>
+            <div className="form-group">
+               <label className="control-label col-sm-1" htmlFor='originAutoSuggest'>To</label>
+               <AutoSuggest suggestions={this.suggestions}
+                     suggestionRenderer={this.suggestionRenderer}
+                     suggestionValue={this.suggestionValue}
+                     onSuggestionSelected={this.onDestinationSelected}
+                     inputAttributes={{ className: "col-sm-12 form-control", placeholder: "East Croydon", id: "originAutoSuggest"}}/>
+            </div>
             <div className="row">
               <div className="col-xs-4">
                 <Input type="select"
@@ -66,7 +90,7 @@ var TrainInputs = React.createClass({
                        wrapperClassName="col-sm-8"/>
               </div>
               <div className="col-xs-4 col-xs-offset-1">
-                  <ButtonInput wrapperClassName="col-sm-6 btn-block" style={{width:"100%"}} type="submit"  onClick={this.handleSubmit}>Find Trains</ButtonInput>
+                  <ButtonInput wrapperClassName="col-sm-6 btn-block" style={{width: "100%"}} type="submit" onClick={this.handleSubmit}>Find Trains</ButtonInput>
               </div>
             </div>
           </form>
