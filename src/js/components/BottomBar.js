f3fe26c5adf8b39e3fa746606be0e5adc8541153
George Goldberg <george@fasteroute.com>
2015-09-26 15:13:38 +0100

    Make the bottom footer responsive and the NRE attribution.

--- src/js/components/BottomBar.js
+++ src/js/components/BottomBar.js
@@ -5,14 +5,16 @@ var React = require('react');
 var BottomBar = React.createClass({
 
     render: function() {
-        return (
-          <nav className="navbar navbar-default navbar-fixed-bottom">
-            <div id="attribution">
-              <a href="http://www.nationalrail.co.uk"><img src="/img/Powered_by_NRE.png"/></a>
-            </div>
-            <div className="container-fluid">
-            </div>
-          </nav>
+      return (
+          <footer>
+            <nav className="navbar navbar-default bottom-navbar">
+              <div id="attribution">
+                <a href="http://www.nationalrail.co.uk"><img src="/img/Powered_by_NRE.png"/></a>
+              </div>
+              <div className="container-fluid">
+              </div>
+            </nav>
+          </footer>
         );
     }
 
